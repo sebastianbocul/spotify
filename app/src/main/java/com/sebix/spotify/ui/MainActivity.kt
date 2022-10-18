1904f662cb0c26fc8e7d8faee666e097b60dd8be
sebastianbocul <seba23495@gmail.com>
2022-10-18 21:43:05 +0200

    Created MainActivity, changed launcher icon

--- app/src/main/java/com/sebix/spotify/ui/MainActivity.kt
+++ app/src/main/java/com/sebix/spotify/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sebix.spotify
+package com.sebix.spotify.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sebix.spotify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
